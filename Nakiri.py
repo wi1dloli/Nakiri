f2f9b2fe281bc3ed98419cdbcf7239262230fe1e
Yato <tylergoodmen312@gmail.com>
2022-08-19 11:21:45 -0400

    rem .env, gitignore, bot class switch, req file

--- Nakiri.py
+++ Nakiri.py
@@ -3,14 +3,14 @@ import os
 
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+bot = commands.AutoShardedBot(intents=discord.Intents.all())
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f"{bot.user} has connected to Discord!")
 
-client.run(TOKEN)
+bot.run(os.getenv("DISCORD_TOKEN"))

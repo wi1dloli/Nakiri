5cc55f65fcaf8ab419edb3696421f39cba4526c4
wi1dloli <pugskateboard1704@gmail.com>
2022-08-22 19:13:53 -0400

    def setup (idk what that does)

--- cogs/main.py
+++ cogs/main.py
@@ -4,6 +4,10 @@ from discord.ext import commands
 class Main(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+
     @commands.command()
     async def hello(self, ctx: commands.Context):
         await ctx.send("Hello World")
+
+def setup(bot:commands.Bot):
+    bot.add_cog(Main(bot))
